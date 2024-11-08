8ba24a21fd864e4d52eaf3b7f6595f6356fbf4a2
Acristap <aadriaanaa85@gmail.com>
2024-11-08 14:11:26 -0500

    Delete Clase_3 directory

--- Clase_3/Resta.py
+++
@@ -1,7 +0,0 @@
-Número_1= input('ingrese primer número:')
-print(Número_1)
-Número_2= input('ingrese segundo número:')
-print(Número_2)
-
-resta= int(Número_1) - int(Número_2) 
-print('La resta es:',resta)
