c2a70d213e5de70d9941452a2666e5780c39cf54
Acristap <aadriaanaa85@gmail.com>
2024-11-08 14:11:37 -0500

    Delete Clase_4 directory

--- Clase_4/Tuplas.py
+++
@@ -1,5 +0,0 @@
-colores = ("Amarillo", "Azul", "Rojo")
-print(colores[1])
-
-for color in colores:
-    print(color)
--- Clase_4/promedio.py
+++
@@ -1,6 +0,0 @@
-numeros = [10,4,6,5,4,9,8,7,3,2,11,15,13,2,0]
-print (numeros)
-suma=(sum(numeros))
-cantidad= (len(numeros))
-promedio_numero= suma / cantidad
-print(promedio_numero)
