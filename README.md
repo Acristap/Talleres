bac0bd69af16174d5f62df29a4a95ffbd043e0a4
Acristap <aadriaanaa85@gmail.com>
2024-11-08 12:40:13 -0500

    diccionarios

--- README.md
+++
@@ -1,1 +0,0 @@
-﻿# Clases_phyton
---
+++ clase_5/tupla_dias_semana.py
@@ -0,0 +1,6 @@
+dias_semana= ("lunes", "martes", "miercoles","jueves", "viernes", "sabado", "domingo")
+print(dias_semana)
+print(dias_semana[2].upper())
+print(dias_semana[6].capitalize())
+
+
