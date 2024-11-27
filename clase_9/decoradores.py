57bd5fef4715800201786e348b960d6e72b33026
Acristap <aadriaanaa85@gmail.com>
2024-11-26 22:05:03 -0500

    clase 9

---
+++ clase_9/decoradores.py
@@ -0,0 +1,40 @@
+import time
+
+def medir_tiempo(funcion):
+    def wrapper(*args, ** kwargs):
+        inicio = time.time() # Momento en que se inicia la ejecución de la función
+        resultado = funcion(*args, ** kwargs) # Llamada a la funcion original
+        fin = time.time() # Momento en que se finaliza la ejecución de la función
+        tiempo_transcurrido = fin - inicio
+        print(f"La función {funcion.__name__} tardó {tiempo_transcurrido} segundos en ejecutarse.")
+        return resultado
+    return wrapper
+
+@medir_tiempo
+def operacion_costosa(n):
+    # Simulamos una operación costosa con un bucle
+    suma = 0
+    for i in range(n):
+        suma += i
+    return suma
+
+@medir_tiempo
+def operacion_costosa_n2(n):
+    # Simulamos una operación costosa con un bucle
+    suma = 0
+    for i in range(n):
+        for j in range(n):
+            suma += i + j
+    return suma
+
+# Ejemplo de uso
+print("empieza")
+resultado = operacion_costosa(10000)
+print("El resultado de la operación costosa es:", resultado)
+print("termina")
+
+
+print("empieza")
+resultado = operacion_costosa_n2(10000)
+print("El resultado de la operación costosa es:", resultado)
+print("termina")
---
+++ clase_9/ejemplodecoradores.py
@@ -0,0 +1,29 @@
+def manejador_errores(funcion):
+    def wrapper(*args, **kwargs):
+        try:
+            resultado = funcion(*args, **kwargs) # Llamada a la funcion original
+        except:
+            resultado = "hubo un error"
+        return resultado
+    return wrapper
+
+## caso de errores
+@manejador_errores
+def division(a, b):
+    return a / b
+
+@manejador_errores
+def sumar(a, b):
+    return a + b
+
+@manejador_errores
+def restar(a, b):
+    return a - b
+
+#resultado = division(1, 0)
+
+resultado = sumar(1, 15)
+print(resultado)
+
+resultado = sumar(1, "w")
+print(resultado)
